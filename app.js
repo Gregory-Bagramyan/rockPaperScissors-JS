a11dff34bc1b506e938e5994cf1382c1572dfa5f
Gregory <85247790+Gregory-Bagramyan@users.noreply.github.com>
2022-12-23 20:41:45 +0100

    update

--- app.js
+++ app.js
@@ -4,23 +4,53 @@ document.addEventListener("DOMContentLoaded", () => {
   const scissors = document.querySelector(".scissors");
 
   let playerChoice = null;
+  const posibleChoices = ["rock", "paper", "scissors"];
+  let robotChoice = null;
 
-  function rockPlayedHandler() {
+  function delay(milliseconds) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, milliseconds);
+    });
+  }
+
+  function robotChoosing() {
+    return posibleChoices[Math.floor(Math.random() * posibleChoices.length)];
+  }
+
+  async function rockPlayedHandler() {
     playerChoice = "rock";
-    const output = `you chose ${playerChoice} bruv`;
-    document.querySelector(".display").innerHTML = output;
+    const playerChoiceDisplay = `you chose ${playerChoice} bruv`;
+    document.querySelector(".player-choice-display").innerHTML =
+      playerChoiceDisplay;
+    robotChoice = robotChoosing();
+    await delay(800);
+    const robotChoiceDisplay = `I chose ${robotChoice} dawg`;
+    document.querySelector(".robot-choice-display").innerHTML =
+      robotChoiceDisplay;
   }
 
-  function paperPlayedHandler() {
+  async function paperPlayedHandler() {
     playerChoice = "paper";
-    const output = `you chose ${playerChoice} fam`;
-    document.querySelector(".display").innerHTML = output;
+    const playerChoiceDisplay = `you chose ${playerChoice} fam`;
+    document.querySelector(".player-choice-display").innerHTML =
+      playerChoiceDisplay;
+    robotChoice = robotChoosing();
+    await delay(800);
+    const robotChoiceDisplay = `I chose ${robotChoice} brruv`;
+    document.querySelector(".robot-choice-display").innerHTML =
+      robotChoiceDisplay;
   }
 
-  function scissorsPlayedHandler() {
+  async function scissorsPlayedHandler() {
     playerChoice = "scissors";
-    const output = `you chose ${playerChoice} dawg`;
-    document.querySelector(".display").innerHTML = output;
+    const playerChoiceDisplay = `you chose ${playerChoice} dawg`;
+    document.querySelector(".player-choice-display").innerHTML =
+      playerChoiceDisplay;
+    robotChoice = robotChoosing();
+    await delay(800);
+    const robotChoiceDisplay = `I chose ${robotChoice} fam`;
+    document.querySelector(".robot-choice-display").innerHTML =
+      robotChoiceDisplay;
   }
 
   rock.addEventListener("click", rockPlayedHandler);
